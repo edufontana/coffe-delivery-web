f54f6a25db922a824245f621a61a62c72c43683d
eduardo <edufontana7@gmail.com>
2023-01-28 19:12:55 -0300

    style: section title and adress form component

---
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AdressForm.tsx
@@ -0,0 +1,9 @@
+import { AdressFormContainer } from './styles'
+
+export function AdressForm() {
+  return (
+    <AdressFormContainer>
+      <input type="text" />
+    </AdressFormContainer>
+  )
+}
--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,14 +1,27 @@
 import { TitleText } from '../../../../components/Typography'
+import { SectionTitle } from '../SectionTitle'
 import { CompleteOrderFormContainer, FormSectionContainer } from './styles'
+import { MapPinLine } from 'phosphor-react'
+import { useTheme } from 'styled-components'
+import { AdressForm } from './AdressForm'
 
 export function CompleteOrderForm() {
+  const { colors } = useTheme()
+
   return (
     <CompleteOrderFormContainer>
       <TitleText size="xs" color="subtitle">
         Complete seu pedido
       </TitleText>
 
-      <FormSectionContainer></FormSectionContainer>
+      <FormSectionContainer>
+        <SectionTitle
+          title="Endereço de entrega"
+          subtitle="Informe o endereço onde deseja receber o pedido"
+          icon={<MapPinLine size={22} color={colors['brand-yellow-dark']} />}
+        />
+        <AdressForm />
+      </FormSectionContainer>
     </CompleteOrderFormContainer>
   )
 }
